f33d7b63f9f4732c4d717af1a304acbce597fbc1
Wahid Maruf <wahid.maruf@ibm.com>
2022-06-04 23:12:07 -0300

    Add stateful widget | Update widget state upon tapping

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -15,17 +16,54 @@ class MyApp extends StatelessWidget {
       home: AnnotatedRegion<SystemUiOverlayStyle>(
         value: SystemUiOverlayStyle.light,
         child: Scaffold(
-          backgroundColor: Colors.deepPurple,
-          body: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Center(child: Text("Hello World"))
-              ],
-            ),
+          backgroundColor: Colors.blueGrey.shade900,
+          appBar: AppBar(
+            title: const Text("Dice"),
+            backgroundColor: Colors.blueGrey,
+          ),
+          body: const SafeArea(
+            child: DicePage(),
           ),
         ),
       ),
+    );
+  }
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(children: [
+        Expanded(
+            child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    leftDiceNumber = Random().nextInt(6) + 1;
+                    rightDiceNumber = Random().nextInt(6) + 1;
+                  });
+                },
+                child: Image.asset("images/dice$leftDiceNumber.png"))),
+        Expanded(
+            child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    leftDiceNumber = Random().nextInt(6) + 1;
+                    rightDiceNumber = Random().nextInt(6) + 1;
+                  });
+                },
+                child: Image.asset("images/dice$rightDiceNumber.png"))),
+      ]),
     );
   }
 }
